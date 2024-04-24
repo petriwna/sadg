dcfc1b414aac23be29699d40f9b0f104c5786038
petriwna <petriwna43@gmail.com>
2024-04-24 20:00:03 +0300

    add description product 2mm to modal

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -4,6 +4,8 @@ export class Modal {
         this.modalBtnClose = document.querySelector(closeBtnSelector);
         this.modalTitle = document.querySelector('.modal__title');
         this.modalCode = document.querySelector('.modal__code');
+        this.modalPrice = document.querySelector('.modal__price');
+        this.modalDescription = document.querySelector('.modal__description')
         this.handleClickClose();
     }
 
@@ -14,15 +16,33 @@ export class Modal {
 
         if (isBackdropHidden) {
             this.getCategory(card)
-            console.log(card.dataset.id);
+        } else {
+            this.close();
         }
     }
 
     getCategory(card) {
         const cardTitle = card.querySelector('.card__title').textContent;
         const cardCode = card.querySelector('.card__code').textContent;
+        const cardPriceOld = card.querySelector('.price__old').textContent;
+        const cardPriceNew = card.querySelector('.price__new').textContent;
+        const cardDescription = card.querySelector('.product');
+        const cardDescriptionCopy = cardDescription.cloneNode(true);
+
+        cardDescriptionCopy.classList.remove('visually-hidden');
         this.modalTitle.textContent = cardTitle;
         this.modalCode.textContent = cardCode;
+        this.modalPrice.innerHTML = `
+            <p class="price__new">${cardPriceNew}</p>
+            <p class="price__old">${cardPriceOld}</p>
+        `;
+        this.modalDescription.appendChild(cardDescriptionCopy);
+    }
+
+    close() {
+        const cardDescriptionCopy = this.modalDescription.querySelector('.product');
+
+        this.modalDescription.removeChild(cardDescriptionCopy)
     }
 
     handleClickClose() {
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
 import {Catalog} from "./Catalog";
 
 export function init() {
-    console.log("init");
+    document.getElementById('up').addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    })
     new Catalog();
 }
 

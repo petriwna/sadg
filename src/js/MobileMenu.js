f50969a3be03b090df7212a283c47336f9344b90
petriwna <petriwna43@gmail.com>
2024-04-26 20:31:36 +0300

    add slider to catalog

---
+++ src/js/MobileMenu.js
@@ -0,0 +1,22 @@
+export class MobileMenu {
+  constructor() {
+    this.menuBtnOpen = document.querySelector('.menu-btn-open');
+    this.menuBtnClose = document.querySelector('.menu-btn-close');
+  }
+
+  toggleMenu() {
+    document.querySelector('.mobile-menu').classList.toggle('is-open');
+  }
+
+  disableScroll() {
+    document.body.classList.toggle('is-scroll-disabled');
+  }
+
+  addListener() {
+    this.menuBtnOpen.addEventListener('click', this.toggleMenu);
+    this.menuBtnClose.addEventListener('click', this.toggleMenu);
+
+    this.menuBtnOpen.addEventListener('click', this.disableScroll);
+    this.menuBtnClose.addEventListener('click', this.disableScroll);
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,14 @@
 import '../scss/main.scss';
 
+import Swiper from 'swiper/bundle';
+
 import { Catalog } from './Catalog';
+import { MobileMenu } from './MobileMenu';
 import { toggleBtn } from './utils';
 
 export function init() {
+  new MobileMenu().addListener();
+
   const moveTopBtn = document.getElementById('up');
 
   moveTopBtn.addEventListener('click', () => {
@@ -17,6 +22,27 @@ export function init() {
   window.addEventListener('scroll', () => toggleBtn(moveTopBtn));
 
   new Catalog();
+  const mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    breakpoints: {
+      600: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1280: {
+        slidesPerView: 4,
+      },
+    },
+    loop: true,
+    pagination: {
+      el: '.pagination',
+      bulletClass: 'pagination__button',
+      bulletActiveClass: 'pagination__button--active',
+    },
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);

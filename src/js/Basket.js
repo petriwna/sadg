5b25fd0489e5d315ce6f0af3d5e98aa2d2839ea2
petriwna <petriwna43@gmail.com>
2024-04-30 18:41:41 +0300

    refact code ProductModal

--- src/js/Basket.js
+++ src/js/Basket.js
@@ -4,25 +4,15 @@ export class Basket {
   constructor() {
     this.basketFab = document.querySelector('#basket');
     this.counterFab = document.getElementById('basket-count');
-    this.counterInput = document.getElementById('order-counter');
-    this.counterInputValue = Number(this.counterInput.value);
-    this.incrementBtn = document.querySelector('.plus');
-    this.decrementBtn = document.querySelector('.minus');
+
     this.modal = new Modal('#modal-basket', '#close-basket');
 
     this.counter = 0;
     this.basket = {};
     this.addClickListeners();
-    this.addInputListener();
   }
 
   addClickListeners() {
-    this.incrementBtn.addEventListener('click', () => {
-      this.handleClickIncrement();
-    });
-    this.decrementBtn.addEventListener('click', () => {
-      this.handleClickDecrement();
-    });
     this.basketFab.addEventListener('click', () => this.handleClickFab());
   }
 
@@ -33,28 +23,5 @@ export class Basket {
   addProductToBasket(card) {
     this.counter = this.counter + 1;
     this.counterFab.innerText = this.counter;
-  }
-
-  addInputListener() {
-    this.counterInput.addEventListener('input', () => {
-      const newValue = parseInt(this.counterInput.value);
-      if (!isNaN(newValue) && newValue >= 1 && newValue <= 999) {
-        this.counterInputValue = newValue;
-      }
-    });
-  }
-
-  handleClickIncrement() {
-    if (this.counterInputValue < 999) {
-      this.counterInputValue += 1;
-      this.counterInput.value = this.counterInputValue;
-    }
-  }
-
-  handleClickDecrement() {
-    if (this.counterInputValue > 1) {
-      this.counterInputValue -= 1;
-      this.counterInput.value = this.counterInputValue;
-    }
   }
 }
--- src/js/ProductModal.js
+++ src/js/ProductModal.js
@@ -1,3 +1,4 @@
+import { Basket } from './Basket';
 import { Modal } from './Modal';
 
 export class ProductModal extends Modal {
@@ -8,12 +9,30 @@ export class ProductModal extends Modal {
     this.modalCode = document.querySelector('.modal__code');
     this.modalPrice = document.querySelector('.modal__price');
     this.modalDescription = document.querySelector('.modal__description');
+
+    this.counterInput = document.getElementById('order-counter');
+    this.incrementBtn = document.querySelector('.plus');
+    this.decrementBtn = document.querySelector('.minus');
+    this.orderBtn = document.getElementById('order-modal');
+
+    this.basket = new Basket();
     this.cardDescriptionCopy = null;
+    this.counterInputValue = 1;
 
     this.handleClickClose = this.handleClickClose.bind(this);
+    this.handleClickOrderBtn = this.handleClickOrderBtn.bind(this);
 
+    this.setupEventListeners();
+  }
+
+  setupEventListeners() {
     this.closeBtn.addEventListener('click', this.handleClickClose);
     this.backdrop.addEventListener('click', this.handleClickClose);
+    this.orderBtn.addEventListener('click', this.handleClickOrderBtn);
+
+    this.incrementBtn.addEventListener('click', () => this.handleClickIncrement());
+    this.decrementBtn.addEventListener('click', () => this.handleClickDecrement());
+    this.counterInput.addEventListener('input', () => this.handleCounterInput());
   }
 
   openWithProduct(card) {
@@ -22,30 +41,72 @@ export class ProductModal extends Modal {
   }
 
   getProductDescription(card) {
-    const cardTitle = card.querySelector('.card__title').textContent;
-    const cardCode = card.querySelector('.card__code').textContent;
-    const cardPriceOld = card.querySelector('.price__old').textContent;
-    const cardPriceNew = card.querySelector('.price__new').textContent;
-    const cardDescription = card.querySelector('.product');
-    this.cardDescriptionCopy = cardDescription.cloneNode(true);
+    const { title, code, priceNew, priceOld, description } = this.extractProductDetails(card);
 
-    this.cardDescriptionCopy.classList.remove('visually-hidden');
-    this.modalTitle.textContent = cardTitle;
-    this.modalCode.textContent = cardCode;
+    this.cardDescriptionCopy = description;
+
+    this.updateModalContent(title, code, priceNew, priceOld, description);
+  }
+
+  extractProductDetails(card) {
+    const title = card.querySelector('.card__title').textContent;
+    const code = card.querySelector('.card__code').textContent;
+    const priceOld = card.querySelector('.price__old').textContent;
+    const priceNew = card.querySelector('.price__new').textContent;
+    const description = card.querySelector('.product').cloneNode(true);
+    description.classList.remove('visually-hidden');
+
+    return { title, code, priceNew, priceOld, description };
+  }
+
+  updateModalContent(title, code, priceNew, priceOld, description) {
+    this.modalTitle.textContent = title;
+    this.modalCode.textContent = code;
     this.modalPrice.innerHTML = `
-            <p class="price__new">${cardPriceNew}</p>
-            <p class="price__old">${cardPriceOld}</p>
-        `;
-    this.modalDescription.appendChild(this.cardDescriptionCopy);
+      <p class="price__new">${priceNew}</p>
+      <p class="price__old">${priceOld}</p>
+    `;
+    this.modalDescription.appendChild(description);
   }
 
   handleClickClose() {
     this.removeDescription();
+    this.resetCounter();
   }
 
   removeDescription() {
     if (this.cardDescriptionCopy) {
       this.cardDescriptionCopy.remove();
     }
+  }
+
+  resetCounter() {
+    this.counterInput.value = 1;
+    this.counterInputValue = 1;
+  }
+
+  handleCounterInput() {
+    const newValue = parseInt(this.counterInput.value);
+    if (!isNaN(newValue) && newValue >= 1 && newValue <= 999) {
+      this.counterInputValue = newValue;
+    }
+  }
+
+  handleClickIncrement() {
+    if (this.counterInputValue < 999) {
+      this.counterInputValue += 1;
+      this.counterInput.value = this.counterInputValue;
+    }
+  }
+
+  handleClickDecrement() {
+    if (this.counterInputValue > 1) {
+      this.counterInputValue -= 1;
+      this.counterInput.value = this.counterInputValue;
+    }
+  }
+
+  handleClickOrderBtn() {
+    console.log('order');
   }
 }

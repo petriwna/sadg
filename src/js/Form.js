a633a509c9212701664d5b1976670cc499b90fb2
petriwna <petriwna43@gmail.com>
2024-05-15 22:18:05 +0300

    add alert

--- src/js/Form.js
+++ src/js/Form.js
@@ -161,8 +161,6 @@ export class Form {
       }
       this.renderAlert('ok', 'Дякуємо за вашу заявку!');
     } catch (error) {
-      this.renderAlert('error', 'Заявка не відправлена! Спробуйте пізніше.');
-
       sendError(error, 'Заявка не відправлена!');
       this.renderAlert('error', 'Заявка не відправлена! Спробуйте пізніше.');
     }
@@ -170,10 +168,39 @@ export class Form {
 
   renderAlert(status, message) {
     if (status === 'ok') {
-      console.log(message);
+      this.createAlert(status, message);
     } else {
-      console.log(message);
+      this.createAlert(status, message);
     }
+  }
+
+  createAlert(type, text) {
+    const notificationBox = document.querySelector('main');
+
+    const component = document.createElement('div');
+    component.className = `alert alert__${type}`;
+    component.innerHTML = `
+      <svg class='alert__icon' width='24' height='24'>
+        <use href='public/images/icons.svg#icon-${type}'></use>
+      </svg>
+      <p class='alert__text'>${text}</p>`;
+    notificationBox.appendChild(component);
+    setTimeout(() => {
+      component.classList.remove('opacity-0');
+      component.classList.add('opacity-1');
+    }, 1);
+    setTimeout(() => {
+      component.classList.remove('opacity-1');
+      component.classList.add('opacity-0');
+      component.style.margin = 0;
+      component.style.padding = 0;
+    }, 2000);
+    setTimeout(() => {
+      component.style.setProperty('height', '0', 'important');
+    }, 2100);
+    setTimeout(() => {
+      notificationBox.removeChild(component);
+    }, 2700);
   }
 
   clearForm() {
